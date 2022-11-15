2196facdc0fb838a256f6c9b0ceed0d7fbbc3430
Gedion <gedion.demmissie@pacshealth.com>
2022-11-15 13:35:17 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using SimpleRestApiClientApplication.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace SimpleRestApiClientApplication
+{
+    /// <summary>
+    /// Program to drive the SimpleRestApi service
+    /// </summary>
+    class Program
+    {        
+        private static IConfigurationBuilder builder = new ConfigurationBuilder()
+                                                          .SetBasePath(Directory.GetCurrentDirectory())
+                                                          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                                          .AddEnvironmentVariables();
+        public static void  Main(string[] args)
+        {
+            var restApiClientConfig = new RestApiClientConfig();
+            builder.Build().GetSection("RestApiClient").Bind(restApiClientConfig);
+
+            var studentsPayload =  RestApiClient<List<StudentPayload>>.Get(url:restApiClientConfig.EndPointUrl, contentType:restApiClientConfig.ContentType , userAgent:restApiClientConfig.UserAgent).Result;
+            foreach (var  studentPayload in studentsPayload)
+            {
+                Console.WriteLine($"Name: {studentPayload.name}.");
+                Console.WriteLine($"Street: {studentPayload.street}.");
+                Console.WriteLine($"City: {studentPayload.city}.");
+                Console.WriteLine($"State: {studentPayload.state}.");
+                Console.WriteLine($"Zip: {studentPayload.zip}.");
+                Console.WriteLine();
+
+            }
+            
+            Console.ReadKey();
+        }       
+    }
+}
---
+++ StudentPayload.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SimpleRestApiClientApplication
+{
+   public  class StudentPayload
+    {
+        [DataMember(Name = "name")]
+        public string name { get; set; }
+        [DataMember(Name = "street")]
+        public string street { get; set; }
+        [DataMember(Name = "city")]
+        public string city { get; set; }
+        [DataMember(Name = "state")]
+        public string state { get; set; }
+        [DataMember(Name = "zip")]
+        public string zip { get; set; }
+    }
+}
